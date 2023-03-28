00b2ff2543a0f76546446bd2d945bc3ef5726a68
Shubham Jaiswar <shubhamjaiswar08@gmail.com>
2023-03-28 16:51:50 +0530

    Adding RomanToNumber File

---
+++ C++/Basic/RomanToNumber.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    //int I=1,V=5,X=10,L=50,C=100,D=500,M=1000;
+    int num=0;
+    string st;
+    input:
+    cout<<"Enter a Roman Number : ";
+    cin>>st;
+    int n=st.size();
+    int arr[n];
+    for(int i=0;i<n;i++){
+        switch(st[i]){
+            case 'I':
+                arr[i]=1;
+                break;
+            case 'V':
+                arr[i]=5;
+                break;
+            case 'X':
+                arr[i]=10;
+                break;
+            case 'L':
+                arr[i]=50;
+                break;
+            case 'C':
+                arr[i]=100;
+                break;
+            case 'D':
+                arr[i]=500;
+                break;
+            case 'M':
+                arr[i]=1000;
+                break;
+            default:
+                cout<<"Invalid Input!!!\nTry Again...\n"<<endl;
+            goto input;
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(i<n-1){
+            if(arr[i]<arr[i+1])
+                num-=arr[i];
+            else
+                num+=arr[i];
+        }
+        else
+            num+=arr[i];
+    }
+    cout<<"Number : "<<num<<endl;
+    return 0;
+}
